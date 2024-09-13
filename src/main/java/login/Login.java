6e8f6e93da97b92d345bb56587f8cf43b9f3b6ce
pic1085 <incheol1085@gmail.com>
2024-09-13 17:49:42 +0900

    BP-83 feat : Login Main 클래스

---
+++ src/main/java/login/Login.java
@@ -0,0 +1,9 @@
+package org.example.login;
+
+import javax.swing.*;
+
+public class Login {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LoginUI());
+    }
+}
