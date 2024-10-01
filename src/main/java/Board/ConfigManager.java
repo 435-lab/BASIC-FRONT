6c3707d9fb56b035838082bfdf7543c88cf66db0
pic1085 <incheol1085@gmail.com>
2024-10-01 19:48:44 +0900

    BP-98 feat : 서버 주소 연결 작업

---
+++ src/main/java/Board/ConfigManager.java
@@ -0,0 +1,30 @@
+package org.example.Board;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+    private Properties properties;
+
+    public ConfigManager() {
+        properties = new Properties();
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("설정 파일을 찾을 수 없습니다.");
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("설정 파일을 로드하는 중 오류가 발생했습니다.");
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
