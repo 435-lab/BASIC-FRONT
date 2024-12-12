34b514457af0aca2bb96701e7e58330b4a355584
kihoon <choien12345679@gmail.com>
2024-12-12 20:47:53 +0900

    fix : Test용 NewsCards 파일 삭제

--- src/main/java/news/NewsCards.java
+++
@@ -1,9 +0,0 @@
-package org.example.news;
-
-import javax.swing.*;
-
-public class NewsCards {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(NewsUI::new);
-    }
-}
